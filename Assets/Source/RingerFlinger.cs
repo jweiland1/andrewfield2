70366c8ed3101f081d390b0c720456ce89acfb17
illsaveus <illsaveus@outlook.com>
2021-05-06 17:42:49 -0700

    Ringer flinger randomizes direction & force, ring have torus collider now

--- Assets/Source/RingerFlinger.cs
+++ Assets/Source/RingerFlinger.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RingerFlinger : MonoBehaviour
-{
-    [SerializeField] private int inventorySize = 20;
-    [SerializeField] private Inventory inventory;
-    [SerializeField] private GameObject[] attachmentPoints;
+{    
+    [SerializeField] private InventoryContainer inventoryContainer;
+    [SerializeField] GameObject launcherDirection;
+    [SerializeField] private float launchPower = 5f;
+    [SerializeField] private float launchRange = 5f;
+    [SerializeField] private float angleRange = 5f;
 
-    // Start is called before the first frame update
-    void Start()
+    public void LaunchObject()
     {
-        inventory = new Inventory(size: inventorySize, attachmentPoints);
-    }
+        // check ring count
+        if (inventoryContainer.inventory.cache > 0)
+        {
+            // launch ring using current Flywheel Power as force
+            var nextObject = inventoryContainer.inventory.PopFromInventory();
+            if(nextObject)
+            {
+                nextObject.transform.parent = null;
+                nextObject.transform.position = launcherDirection.transform.position;
+                nextObject.transform.rotation = launcherDirection.transform.rotation;
+                //launch in direction the launcher is facing
+                var rb = nextObject.GetComponent<Rigidbody>();
+                rb.isKinematic = false;
+                rb.mass = rb.GetComponent<ScoringObjectDataContainer>().data.Mass;
+                nextObject.GetComponentInChildren<Collider>().enabled = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                float randomLaunchPower = Random.Range(launchPower - launchRange, launchPower + launchRange);
+                Vector3 randomAngle = new Vector3(
+                    Random.Range(launcherDirection.transform.forward.x - angleRange, launcherDirection.transform.forward.x + angleRange),
+                    Random.Range(launcherDirection.transform.forward.y - angleRange, launcherDirection.transform.forward.y + angleRange),
+                    Random.Range(launcherDirection.transform.forward.z - angleRange, launcherDirection.transform.forward.z + angleRange)
+                    );
+
+                rb.AddForce(randomAngle * randomLaunchPower);
+            }
+        }            
     }
 }
